3ae038270489e46232b2d8fd0d5baad0b4f89bef
McKeegan Curran-Seijo <mcurranseijo@mail.bradley.edu>
2020-11-14 02:54:41 +0000

    finished animal cog

--- cogs/animals.py
+++ cogs/animals.py
@@ -17,22 +17,42 @@ class AnimalCog(commands.Cog,name='animals'):
       url = 'https://api.thedogapi.com/v1/images/search'
 
       response = urllib.request.urlopen(url)
-      data = json.load(response)[0]['breeds']
+      data = json.load(response)[0]
+      image = data['url']
+      try:
+        data = data['breeds'][0]
+      except IndexError:
+        data = data['breeds']
       if not data:
         await self.dog(ctx)
       else:
-        await ctx.send(data)
+        try:
+          fields = {
+            ('Weight',f"{data['weight']['imperial']}lbs.",True),
+            ('Height',f"{data['height']['imperial']}in.",True),
+            ('Breed Group',data['breed_group'] if data['breed_group'] else "N/A",True),
+            ('Life Span',data['life_span'],True),
+            ('Bred for',data['bred_for'] if data['bred_for'] else 'N/A',False),
+            ('Temperament',data['temperament'],False)
+          }
+          embed=discord.Embed(title=data['name'])
+          embed.set_image(url = image)
+          for name,value,inline in fields:
+            embed.add_field(name=name,value=value,inline=inline)
+          await ctx.send(embed=embed)
+        except KeyError:
+          await self.dog(ctx)
 
     @commands.command(pass_contex=True)
     async def cat(self,ctx):
         url = 'https://api.thecatapi.com/v1/images/search'
 
         response = urllib.request.urlopen(url)
-        data = json.load(response)
-        print(data)
-        for f in data:
-          image= f['url']
-        await ctx.send(image)
+        data = json.load(response)[0]['url']
+
+        embed = discord.Embed(title='Enjoy a cat picture')
+        embed.set_image(url=data)
+        await ctx.send(embed=embed)
     
 
 
